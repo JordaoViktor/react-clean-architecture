5686c615a5e55d99c42eb661099d8a2a19dae66a
JordaoViktor <jordaovpinheiro@gmail.com>
2021-05-02 06:03:24 -0300

    refactor: move SUT creation to a factory method

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -1,19 +1,21 @@
-import { HttpPostClient } from "src/data/protocols/http/http-post-client";
+import { HttpPostClientSpy } from "../../test/mock-http-client";
 import { RemoteAuthentication } from "./remote-autentication";
+
+type SutTypes = {
+  sut:RemoteAuthentication
+  httpPostClientSpy: HttpPostClientSpy
+}
+
+const makeSut = (url:string = 'any_url'): SutTypes => {
+  const httpPostClientSpy = new HttpPostClientSpy()
+  const sut = new RemoteAuthentication(url, httpPostClientSpy)
+  return {sut, httpPostClientSpy}
+}
 
 describe('RemoteAuthentication',  () => {
   test('should  call HttpPostClient with correct URL', async () => {
-    class HttpPostClientSpy implements HttpPostClient {
-      url?: string
-      async post (url:string): Promise<void> {
-        this.url = url
-        return Promise.resolve()
-      }
-
-    }
-    const url = 'any_url'
-    const httpPostClientSpy = new HttpPostClientSpy()
-    const sut = new RemoteAuthentication(url, httpPostClientSpy)
+    const url = 'other_url'
+    const {sut, httpPostClientSpy} = makeSut(url)
     await sut.auth()
     expect(httpPostClientSpy.url).toBe(url)
   });
